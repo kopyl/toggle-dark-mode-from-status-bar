ab93e1112fdad3d1eddf2e1f0785eb99c84eddbb
kopyl <oleggggg1997@gmail.com>
2025-04-30 05:54:03 +0300

    Finish the MVP

--- main.swift
+++ main.swift
@@ -1,33 +1,64 @@
 import Cocoa
+import OSAKit
+
+let script = """
+tell application "System Events"
+    tell appearance preferences
+        set dark mode to not dark mode
+    end tell
+end tell
+"""
 
 var mainWindow: NSWindow?
+var statusBarItem: NSStatusItem?
 
-func createMaiWindow() {
-    mainWindow = NSWindow(
-        contentRect: NSRect(x: 0, y: 0, width: 500, height: 300),
-        styleMask: [.titled, .closable, .miniaturizable, .resizable],
-        backing: .buffered, defer: false)
+func runScript() {
+    let script = OSAScript(source: script, language: OSALanguage.default())
+    var error: NSDictionary?
+    script.executeAndReturnError(&error)
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate, NSMenuDelegate {
     
-    let viewController = MainViewController()
-    mainWindow?.contentViewController = viewController
-}
-
-func showMainWindow() {
-    mainWindow?.makeKeyAndOrderFront(nil)
-    mainWindow?.center()
-}
-
-class AppDelegate: NSObject, NSApplicationDelegate {
-    var globalKeyMonitor: Any?
+    private var statusBarMenu: NSMenu?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        createMaiWindow()
-        showMainWindow()
+        addStatusBarItem()
     }
-
+    
+    private func addStatusBarItem() {
+        statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+        
+        guard let button = statusBarItem?.button else { return }
+        button.image = NSImage(named: "status-bar-icon")
+        button.image?.isTemplate = true
+        
+        let menu = NSMenu()
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApp.terminate(_:)), keyEquivalent: "q"))
+        menu.delegate = self
+        
+        self.statusBarMenu = menu
+        
+        button.sendAction(on: [.leftMouseUp, .rightMouseUp])
+        button.target = self
+        button.action = #selector(statusBarItemClicked)
+    }
+    
+    @objc func statusBarItemClicked(sender: NSStatusBarButton) {
+        let event = NSApp.currentEvent!
+        
+        if event.type == .rightMouseUp {
+            statusBarItem?.menu = statusBarMenu
+            sender.performClick(nil)
+            statusBarItem?.menu = nil
+        } else if event.type == .leftMouseUp {
+            
+            runScript()
+        }
+    }
 }
 
-let app = Application.shared
+let app = NSApplication.shared
 let delegate = AppDelegate()
 app.delegate = delegate
 
